150660c5c7567d71277a39180541ea2dd626c10d
zbuckley <zbuckley@gwu.edu>
2019-11-11 14:04:36 -0500

    script for downloading data. installs kaggle python api

---
+++ download_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# assumes you have got the kaggle.json file from kaggle, using these instructions:
+https://github.com/Kaggle/kaggle-api
+
+# put the file under ~/.kaggle/kaggle.json
+if [ ! -f /home/${USER}/.kaggle/kaggle.json ]; then
+	echo "kaggle.json file missing"
+	exit 1
+fi
+
+pip install kaggle --user kaggle
+
+kaggle datasets download  -p Data nih-chest-xrays/sample
