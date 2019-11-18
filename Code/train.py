c603eee8b154d597c5ea78ab2646f45865d1f16e
Ubuntu <ubuntu@ip-172-31-21-114.ec2.internal>
2019-11-18 00:03:43 +0000

    Add comments/todos

--- Code/train.py
+++ Code/train.py
@@ -10,6 +10,7 @@ label_columns = ['No Finding',
        'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture',
        'Support Devices']
 
+# Labels in common with Kaggle:
 # No Finding
 # Pneumonia - 62 images
 # Edema - 118 images
@@ -18,7 +19,7 @@ label_columns = ['No Finding',
 # Pneumothorax - 271 images
 # Atelectasis - 508 images
 
-
+# Maybe similar?
 # Pleural_Thickening - 176 images
 # Effusion - 644 images
 
@@ -27,16 +28,19 @@ print(len(df))
 # Sample 1000 for now
 df = df.head(1000)
 
+# TODO: Look at column 0 as well.  Starting at 1 for manual testing b/c it contains both 0s and 1s
 for label in label_columns[1:]:
     df_type = df[['Path', 'Sex', 'Age', 'Frontal/Lateral', 'AP/PA', label]]
+
+    # Drop unknowns
     df_type = df_type[(df_type[label] == 1) | (df_type[label] == 0)]
 
+    # TODO: If we're dropping unknowns, use Random undersampler
     print(df_type)
     # print(df_type.groupby(label)['Path', 'Sex', 'Age', 'Frontal/Lateral', 'AP/PA'].sum())
-
     # print(df_type.sample(n=10, random_state = 0))
 
-
+    # TODO: This should be removed, or somehow adapted. Only here so testing does not get out-of-hand
     break
 
 
