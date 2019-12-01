6768009a43b928dade17b053266cd0f9d073a43a
zbuckley <zbuckley@gwu.edu>
2019-12-01 18:20:59 -0500

    modified image preprocessing based on Chexpert Competition winner

--- Code/CheXpertDataset.py
+++ Code/CheXpertDataset.py
@@ -2,8 +2,9 @@ import os
 import pandas as pd
 import numpy as np
 import torch
+import cv2
+from PIL import Image
 import tensorflow as tf
-from keras.preprocessing.image import load_img
 from torch.utils.data import Dataset
 
 # %% -------------------------------------- Data Prep ------------------------------------------------------------------
@@ -29,18 +30,13 @@ label_columns = [
     'Support Devices'
 ]
 
-
 class ChexpertDataset(Dataset):
     def __init__(self,
                  csv_file,
                  root_dir,
                  image_transform=None,
                  label_transform=None):
-        df = pd.read_csv(csv_file)
-
-        # Only use frontal images
-        self.df = df[df["Frontal/Lateral"] == "Frontal"]
-
+        self.df = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.label_transform = label_transform
         self.image_transform = image_transform
@@ -50,18 +46,24 @@ class ChexpertDataset(Dataset):
 
     def __getitem__(self, idx):
         image_path = self.df.iloc[idx, 0]
-        im = load_img(os.path.join(self.root_dir, image_path), target_size=(600, 600), interpolation='nearest')
+
+        # Updated based on: https://github.com/jfhealthcare/Chexpert/blob/47e1bf7f8ed2b95c8985ae43801ffffde92fa205/data/dataset.py#L104
+        # Image is grayscale here
+        image = cv2.imread(os.path.join(self.root_dir, image_path), 0)
+        image = Image.fromarray(image)
 
         label = self.df.iloc[idx, 1:]
         if self.label_transform is not None:
             label = self.label_transform(label)
 
         if self.image_transform is not None:
-            im = self.image_transform(im)
+            im = self.image_transform(image)
 
         return im, torch.from_numpy(np.array(label[label_columns].values.tolist()))
 
 
+
+# label Transforms
 class ReplaceNaNTransform:
     def __call__(self, sample):
         return sample[label_columns].fillna(0)
@@ -77,10 +79,3 @@ class UOnesTransform:
     def __call__(self, sample):
         sample = sample[label_columns].replace(-1, 1)
         return sample
-
-class GreyScale_to_RGB:
-    def __call__(self, im):
-        return tf.image.grayscale_to_rgb(
-            im,
-            name=None
-        )
