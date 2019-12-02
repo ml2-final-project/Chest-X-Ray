09c4fbfd39370918cdbd6ff6fbbac1cf28f388b4
josh-bernstein618 <jbernstein@gwmail.gwu.edu>
2019-12-02 20:19:33 +0000

    Initial attempt at loading model

--- Code/test_on_kaggle.py
+++ Code/test_on_kaggle.py
@@ -1,12 +1,28 @@
 from KaggleDataset import KaggleDataset
+from torch.autograd import Variable
 from training_common_utils import image_preprocessing
+import torch
+from torchvision.models import densenet121
+from torch.utils.data import DataLoader
 
-data = KaggleDataset(
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+kaggle_data = KaggleDataset(
     csv_file="../Data/sample/sample_labels.csv",
     root_dir="../Data/sample/images",
     image_transform=image_preprocessing
 )
 
-# TODO For testing, will be removed once we're running model on data
-for i in range(10):
-    print(data[i])
+kaggle_data_loader = DataLoader(kaggle_data)
+
+# Load model
+model_state_dict = torch.load("../Models/model_team8_uzeros_15epoch.pt")
+
+model = densenet121(num_classes=14).load_state_dict(model_state_dict)
+model.eval()
+
+# Classify inputs
+for i, t in kaggle_data_loader:
+    inputs, targets = Variable(i), Variable(t)
+    outputs = model(inputs)
+
