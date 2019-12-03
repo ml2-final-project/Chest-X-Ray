c95a006150d0af65ef78bda305b82060839b562f
josh-bernstein618 <jbernstein@gwmail.gwu.edu>
2019-12-03 19:09:23 +0000

    Refactor

---
+++ Code/Test_Model_On_Chexpert.py
@@ -0,0 +1,17 @@
+from torch.utils.data import DataLoader
+
+from Code.CheXpertDataset import ChexpertDataset
+from Code.testing_common_utils import predict_on_test_data
+from Code.training_common_utils import image_preprocessing, label_preprocessing_uzeros
+
+# TODO Should we be transforming the unknown and blanks here?  Or do we want to take into account when it is unknown?
+test_data = ChexpertDataset(
+        csv_file='../Data/CheXpert-v1.0-small/valid.csv',
+        root_dir='../Data',
+        image_transform=image_preprocessing,
+        label_transform=label_preprocessing_uzeros
+)
+
+test_data_loader = DataLoader(test_data)
+
+predict_on_test_data(test_data_loader)
--- Code/testing_common_utils.py
+++ Code/testing_common_utils.py
@@ -1,8 +1,5 @@
-from KaggleDataset import KaggleDataset
-from training_common_utils import image_preprocessing
 import torch
 from torchvision.models import densenet121
-from torch.utils.data import DataLoader
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -20,15 +17,3 @@ def predict_on_test_data(test_data_loader):
         local_images, local_labels = images.to(device, dtype=torch.float), labels.to(device, dtype=torch.float)
         preds = model(local_images)
         print(preds)
-
-
-# Test on Kaggle
-kaggle_data = KaggleDataset(
-    csv_file="../Data/sample/sample_labels.csv",
-    root_dir="../Data/sample/images",
-    image_transform=image_preprocessing
-)
-
-kaggle_data_loader = DataLoader(kaggle_data)
-
-predict_on_test_data(kaggle_data_loader)
