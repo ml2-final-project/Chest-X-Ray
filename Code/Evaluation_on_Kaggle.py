657098516cc7420225cb0f95687e1d23ca282fb3
josh-bernstein618 <jbernstein@gwmail.gwu.edu>
2019-12-06 16:25:46 +0000

    Uncomment uones

--- Code/Evaluation_on_Kaggle.py
+++ Code/Evaluation_on_Kaggle.py
@@ -4,9 +4,6 @@ import numpy as np
 from Test_Model_On_Kaggle import predict
 from KaggleDataset import LABELS
 from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
-output = torch.from_numpy(np.array([[-2.7946, -2.8696, -2.0371, -0.1423, -3.4820, -1.6071, -2.7869, -3.7146,
-         -1.7468, -2.4265, -0.6332, -4.3280, -3.2801,  0.1334]]))
-ten = torch.from_numpy(np.array([]))
 
 label_columns = [
     'No Finding',
@@ -29,12 +26,12 @@ output_uzeros, labels_uzeros = predict(model_name="uzeros")
 proba_uzeros = [torch.nn.functional.softmax(out).numpy() for out in output_uzeros]
 
 
-# output_uones, labels_uones = predict("uones")
-# proba_uones = [torch.nn.functional.softmax(out).numpy() for out in output_uones]
+output_uones, labels_uones = predict("uones")
+proba_uones = [torch.nn.functional.softmax(out).numpy() for out in output_uones]
 
 compare_df = pd.DataFrame({"Labels": LABELS})
 compare_df["Uzeros"] = [0]*len(compare_df)
-# compare_df["Uones"] = [0]*len(compare_df)
+compare_df["Uones"] = [0]*len(compare_df)
 
 def get_auc_score(row):
     print(row)
@@ -44,9 +41,9 @@ def get_auc_score(row):
     y_actuals = [label[LABELS.index(row.Labels)].numpy() for label in labels_uzeros]
     row["Uzeros"] = roc_auc_score(y_actuals, y_predicts)
     # for uones
-    # y_predicts = [out[label_columns.index(row.Labels)] for out in proba_uones]
-    # y_actuals = [label[label_columns.index(row.Labels)].numpy() for label in labels_uones]
-    # row["Uones"] = roc_auc_score(y_actuals, y_predicts)
+    y_predicts = [out[label_columns.index(row.Labels)] for out in proba_uones]
+    y_actuals = [label[label_columns.index(row.Labels)].numpy() for label in labels_uones]
+    row["Uones"] = roc_auc_score(y_actuals, y_predicts)
     return row
 
 compare_df = compare_df.apply(get_auc_score, axis=1)
