e5d037fd788ee90855c18c0a6d6fbbe51fe5dd85
zbuckley <zbuckley@gwu.edu>
2019-12-07 15:19:35 -0500

    further code cleanup

--- Code/Data_Download.py
+++
@@ -1,9 +0,0 @@
-# Please execute this script only once. The script download several gigabytes of data from AWS S3 bucket
-import os
-
-os.system("sh ../download_kaggle_data.sh")
-os.system("sh ../download_stanford_data.sh")
-os.system("sh ../download_trained_models.sh")
-
-print("The data has been downloaded.")
-print("The unzipped files are present in a directory with name: Data")
